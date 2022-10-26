7f590fecc7fa7e833f31224e70a52ef59c9249e6
arly-0 <77885290+arly-0@users.noreply.github.com>
2022-10-26 23:13:46 +0300

    Delete Task 1.cpp

--- Task 1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "Headers.h"
-
-void PrintDataStructure(HEADER_D* pointer_to_data_structure)
-{
-	printf("Pointer address for struct = %p\n", pointer_to_data_structure);
-}
